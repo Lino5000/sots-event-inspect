d746edb34b40ed3e7f1225666997855c2482a6c0
Lino5000 <bathy2oo2@gmail.com>
2023-02-09 21:58:44 +1100

    Add a script to build some release binaries.

---
+++ build_release.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+##############################################################################
+#
+# Build the program for Linux and Windows, both with and without the
+# `display_compat` feature.
+# I don't provide a pre-built MacOSX binary because 
+#  1) I don't have a Mac to test on
+#  2) Apple makes it really annoying to cross-compile anything for their OS
+#
+# In particular, it creates 4 zip files, each containing a single
+# `sots-event-inspect` binary built with the mentioned rustc target:
+#  - windows.zip: x86_64-pc-windows-gnu
+#  - windows-display-compat.zip: x86_64-pc-windows-gnu
+#  - linux.zip: x86_64-unknown-linux-gnu
+#  - linux-display-compat.zip: x86_64-unknown-linux-gnu
+#
+# Note: This script is intended to be run on a Linux system with mingw-w64
+# installed to allow for Windows cross-compilation. It also needs to have the
+# relevant toolchains installed using rustup:
+#  - `rustup target add x86_64-pc-windows-gnu`
+#  - `rustup target add x86_64-unknown-linux-gnu` (probably not needed on a Linux machine)
+
+# Windows
+echo "Building Windows"
+cargo build --release --target x86_64-pc-windows-gnu
+zip -9 release/windows.zip target/x86_64-pc-windows-gnu/release/sots-event-inspect.exe
+
+echo "Building Windows (display_compat)"
+cargo build --release --target x86_64-pc-windows-gnu --features display_compat
+zip -9 release/windows-display-compat.zip target/x86_64-pc-windows-gnu/release/sots-event-inspect.exe
+
+# Linux
+echo "Building Linux"
+cargo build --release --target x86_64-unknown-linux-gnu
+zip -9 release/linux.zip target/x86_64-unknown-linux-gnu/release/sots-event-inspect
+
+echo "Building Linux (display_compat)"
+cargo build --release --target x86_64-unknown-linux-gnu --features display_compat
+zip -9 release/linux-display-compat.zip target/x86_64-unknown-linux-gnu/release/sots-event-inspect
